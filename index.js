fccd713fcb148625ba2bc7ff7301d1ef59c8b9f0
Codi Kraus <codi.kraus@gmail.com>
2021-09-15 11:26:43 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const names = ['Lisa', 'Kaitlin', 'Jan'];
+
+function writeCards(names, event) {
+    let returnArray = [];
+    event = 'surprise';
+    for (let i = 0; i < names.length; i++) {
+        returnArray[i] = 'Thank you, ' + names[i] + ', for the wonderful ' + event + ' gift!';
+    }
+    return returnArray;
+}
+
+let number = 10;
+function countDown(number) {
+    while(number >= 0) {
+        console.log(number--);
+    }
+}
